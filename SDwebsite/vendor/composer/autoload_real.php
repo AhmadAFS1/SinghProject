dc7cea3c923a66ab3f31a7de85021057708bbdf7
Nathan N <nhatnguyen2807@gmail.com>
2021-02-26 17:18:36 -0600

    5:18 02/26/2021

--- SDwebsite/vendor/composer/autoload_real.php
+++ SDwebsite/vendor/composer/autoload_real.php
@@ -25,7 +25,7 @@ class ComposerAutoloaderInit9db131915055f3cf216cecaca0913f7d
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit9db131915055f3cf216cecaca0913f7d', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit9db131915055f3cf216cecaca0913f7d', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
