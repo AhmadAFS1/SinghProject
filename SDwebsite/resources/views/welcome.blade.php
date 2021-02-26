007052650d3e0228728f76139f16168d18a46ee1
Nathan N <nhatnguyen2807@gmail.com>
2021-02-26 16:30:22 -0600

    4:30 02/26/2021

--- SDwebsite/resources/views/welcome.blade.php
+++ SDwebsite/resources/views/welcome.blade.php
@@ -22,19 +22,6 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
@@ -44,12 +31,25 @@
                         </g>
                     </svg>
                 </div>
-                <div class="ml-4 text-center text-sm text-gray-500 sm:text-left sm:ml-0">
+                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-left sm:ml-0">
                         Ahmad - Ivan - Nathan
                     </div>
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
+                    @if (Route::has('login'))
+                        <div class="ml-4 text-center text-sm text-gray-500 sm:text-center sm:ml-0">
+                            @auth
+                                <a href="{{ url('/home') }}" class="text-lg text-gray-1000 underline">Home</a>
+                            @else
+                                <a href="{{ route('login') }}" class="text-lg text-gray-1000 underline">Log in</a>
+
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}" class="ml-4 text-lg text-gray-1000 underline">Register</a>
+                                @endif
+                            @endauth
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
